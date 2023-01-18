585b2c0f0fda3c1d7ffe8846e270c3e60fdf6987
almon-s <almon.subba@gmail.com>
2023-01-18 12:57:03 +0800

    Initial commit

--- pages/models.py
+++ pages/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Movies(models.Model):
-    movie_title = models.CharField(max_length=50)
-    movie_genres = models.CharField(max_length=50)
-    char_cast = models.CharField(max_length=50)
-    overview = models.CharField(max_length=50)
-    release = models.IntegerField
-    tag_line = models.CharField(max_length=50)
-    avg_rating = models.IntegerField
-    vote_count = models.IntegerField
